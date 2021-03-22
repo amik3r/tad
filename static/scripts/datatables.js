9a657da870f3c5cae84a847099d2a9e852175fd3
amik3r <antalmiklos4@gmail.com>
2021-03-22 12:33:29 +0100

    sad

--- static/scripts/datatables.js
+++ static/scripts/datatables.js
@@ -62,6 +62,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 columnDefs: [
                     { targets: 'no-sort', orderable: false, searchable: false }
                 ],
+                "bFilter": false,
                 orderCellsTop: true,
                 fixedHeader: true
             });
@@ -96,6 +97,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 "language": {
                     "url": "//cdn.datatables.net/plug-ins/1.10.24/i18n/English.json"
                 },
+                "bFilter": false,
                 columnDefs: [
                     { targets: 'no-sort', orderable: false, searchable: false }
                 ],
