7bdd8d63fdf27853787f2c6635147760339741e7
amik3r <antalmiklos4@gmail.com>
2021-03-25 10:48:55 +0100

    asd

--- static/scripts/admin.js
+++ static/scripts/admin.js
@@ -2,6 +2,8 @@ function handleCheckboxSelect(e){
     console.log(e.target.value);
 }
 var boxes = document.getElementsByClassName('checkbox')
-boxes.forEach(element => {
+boxes.toArray()
+var arr = Array.prototype.slice.call( coll, 0 );
+arr.forEach(element => {
     element.addEventListener("change", handleCheckboxSelect(e))
 });
